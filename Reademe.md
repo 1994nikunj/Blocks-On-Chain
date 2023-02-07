6201652cc3f394f6270979a9ca63334b86cc9082
Nikunj Sharma <1994nikunj@gmail.com>
2023-02-07 15:46:25 -0500

    First commit - let's go!

---
+++ Reademe.md
@@ -0,0 +1,20 @@
+# Basic Blockchain Implementation in Python
+
+A basic implementation of a blockchain in Python. The blockchain includes blocks that consist of a list of transactions and a nonce, and the chain is secured through the use of a SHA-256 hash function and proof-of-work.
+
+## Features
+- Adding transactions to the blockchain
+- Mining blocks
+- Checking the validity of the blockchain
+
+## Components
+- Transactions
+- Blocks
+- SHA-256 hash function
+- Proof-of-work
+
+## Usage
+The code demonstrates how transactions can be added to the blockchain and how blocks can be mined. The validity of the blockchain is also checked by verifying the hashes of each block and ensuring the integrity of the previous block's hash.
+
+## Conclusion
+This code serves as a basic foundation for understanding the fundamental components of a blockchain.
---
+++ core.py
@@ -0,0 +1,82 @@
+import hashlib
+import time
+
+
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+
+
+class Block:
+    def __init__(self, previous_hash, transactions, nonce=0):
+        self.previous_hash = previous_hash
+        self.transactions = transactions
+        self.nonce = nonce
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        data = self.previous_hash + str(self.nonce) + str([t.__dict__ for t in self.transactions])
+        sha = hashlib.sha256()
+        sha.update(data.encode('utf-8'))
+        return sha.hexdigest()
+
+    def mine_block(self, difficulty):
+        start_time = time.time()
+        target = '0' * difficulty
+        while self.hash[:difficulty] != target:
+            self.nonce += 1
+            self.hash = self.calculate_hash()
+        end_time = time.time()
+        print(f"Block mined in {end_time - start_time:.2f} seconds")
+
+
+class Blockchain:
+    def __init__(self, difficulty=2):
+        self.chain = [self.create_genesis_block()]
+        self.pending_transactions = []
+        self.difficulty = difficulty
+
+    @staticmethod
+    def create_genesis_block():
+        return Block("0", [], 0)
+
+    def add_block(self, block):
+        self.chain.append(block)
+
+    def mine_pending_transactions(self, miner_address):
+        block = Block(self.get_last_block().hash, self.pending_transactions)
+        block.mine_block(self.difficulty)
+        print(f"Block mined: {block.hash}")
+        self.add_block(block)
+        self.pending_transactions = [Transaction(None, miner_address, 1)]
+
+    def add_transaction(self, transaction):
+        self.pending_transactions.append(transaction)
+
+    def get_last_block(self):
+        return self.chain[-1]
+
+    def is_chain_valid(self):
+        for i in range(1, len(self.chain)):
+            current_block = self.chain[i]
+            previous_block = self.chain[i - 1]
+            if current_block.hash != current_block.calculate_hash():
+                print("Current block hash does not match")
+                return False
+            if previous_block.hash != current_block.previous_hash:
+                print("Previous block hash does not match")
+                return False
+        return True
+
+
+blockchain = Blockchain()
+
+print("Mining block 1...")
+blockchain.add_transaction(Transaction("A", "B", 100))
+blockchain.mine_pending_transactions("miner1")
+
+print("Mining block 2...")
+blockchain.add_transaction(Transaction("C", "D", 10))
+blockchain.mine_pending_transactions("miner2")
